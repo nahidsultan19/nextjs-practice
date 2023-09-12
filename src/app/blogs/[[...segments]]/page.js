76c55ef8a07277ee532244abfc26225187e7dfd0
nahid <sultannahid38@gmail.com>
2023-09-12 15:10:21 +0800

    added dynamic routes, catch-all and segments.

---
+++ src/app/blogs/[[...segments]]/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SingleBlog = ({ params }) => {
+    const [year, id] = params.segments || [];
+    return (
+        <div>
+            <h2>Blog {year || new Date().getFullYear()} for {id}</h2>
+        </div>
+    );
+};
+
+export default SingleBlog; <h2>Blog</h2>
